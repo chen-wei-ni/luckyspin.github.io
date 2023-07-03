604a89607ddd56ba71d926164ca23ce8a9328765
chen-wei-ni <bcg2135@gmail.com>
2023-07-03 15:46:17 +0800

    add audio

--- js/all.js
+++ js/all.js
@@ -33,6 +33,12 @@ let m = document.querySelector('.slot_machine');
 */
 var sm = (function(undefined){
 	let prize = document.querySelector('.prize');
+	let popupMusic = new Audio("music/bn_total_win_long.mp3");
+	let bonusMusic = new Audio("music/bonus_stop_1.mp3");
+	let bonusMusic2 = new Audio("music/bonus_stop_2.mp3");
+	let bonusMusic3 = new Audio("music/bonus_stop_3.mp3");
+	let bonusMusic4 = new Audio("music/bonus_stop_4.mp3");
+	let bonusMusic5 = new Audio("music/bonus_stop_5.mp3");
 	var tMax = 3800, // animation time, ms
 		tMax2 = 1000,
 		tMax3 = 1700,
@@ -59,7 +65,6 @@ var sm = (function(undefined){
 			[b, g,g ,g ,g],
 			[g, g,g ,b ,g],
 		],
-		
 		$reels, $msg, $reels2,
 		start;	
 	function initState(){
@@ -76,8 +81,13 @@ var sm = (function(undefined){
 	function reelInit(){
 			$reels = $('.reel').each(function(i, el){
 			p = Math.floor(Math.random()*5)
-			el.innerHTML = `<div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][3]}"></div><div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div><div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div><div class="a1 bonus_how"><img src="${reels2[p][2]}"><img src="${reels2[p][3]}"></div>
-			<div class="a1 bonus_how"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div><div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div><div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][3]}"></div><div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div><div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div><div class="a1"><img src="${reels2[1][2]}"><img src="${reels2[p][3]}"></div><div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div><div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div>
+			el.innerHTML = `
+			<div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][3]}"></div><div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div><div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div>
+			<div class="a1 bonus_how"><img src="${reels2[p][0]}"><img src="${reels2[p][2]}"></div>
+			<div class="a1 bonus_how"><img src="${reels2[p][1]}"><img src="${reels2[p][3]}"></div>
+			<div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div><div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][3]}"></div>
+			<div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div><div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div>
+			<div class="a1"><img src="${reels2[1][2]}"><img src="${reels2[p][3]}"></div><div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div><div class="a1"><img src="${reels2[p][2]}"><img src="${reels2[p][1]}"></div>
 			<div class="a1"><img src="${reels2[1][2]}"><img src="${reels2[p][1]}"></div>
 			`			
 			// '<div><p>' + reels[i].join('</p><p>') + '</p></div><div><p>' + reels[i].join('</p><p>') + '</p></div><div><p>'  +  reels[i].join('</p><p>') +'</p></div>'
@@ -96,79 +106,96 @@ var sm = (function(undefined){
 		for (var i = 0; i < 5; ++i) {	
 			speeds[i] = Math.random()*10 +15;
 			r[i] = 980 || (Math.random()*3 | 0) * height / 3 + Math.floor(Math.random()*3) ; 
-		} 
-		
+		} 		
 		console.log(speeds)
 		allAnimate();
-
 	}
 	function allAnimate(){
-		// let a = document.querySelectorAll('.a1 div img');
-		// for(let b=0; b<a.length; b++){
-		// 	a[b].classList.add("blur");
-		// }
 		(function animate(now){
+			let bonusBox1 = document.querySelectorAll('.reel1 .bonus_how img');
 			if (!start) start = now;
 			let t = now - start || 0;
-				$reels[0].scrollTop = ((speeds[0] / tMax2 / 10 * (tMax2 - t) * (tMax2 - t) + r[0]) % height)+105| 0;
+				$reels[0].scrollTop = ((speeds[0] / tMax2 / 10 * (tMax2 - t) * (tMax2 - t) + r[0]) % height)+98| 0;
 			if (t < tMax2)
 				requestAnimationFrame(animate);
 			else {
 				start = undefined;
-				// check();
-				// for(let b=0; b<a.length; b++){
-				// 	a[b].classList.remove("blur");
-				// }
+				for(i=0;i<4;i++){
+					if(bonusBox1[i].outerHTML=="<img src=\"img/bonus.png\">"){
+						bonusMusic.play();
+					}
+				}					
 			}
 		}());
 		(function animate2(now2){
+			let bonusBox1 = document.querySelectorAll('.reel2 .bonus_how img');
 			if (!start2) start2 = now2;
 			let t = now2 - start2 || 0;
-				p=$reels[1].scrollTop = ((speeds[1] / tMax3 / 10 * (tMax3 - t) * (tMax3 - t) + r[2]) % height)+105 | 0;
+				p=$reels[1].scrollTop = ((speeds[1] / tMax3 / 10 * (tMax3 - t) * (tMax3 - t) + r[2]) % height)+98 | 0;
 			if (t < tMax3)
 				requestAnimationFrame(animate2);
 			else {
 				start2 = undefined;
-
+				for(i=0;i<4;i++){
+					if(bonusBox1[i].outerHTML=="<img src=\"img/bonus.png\">"){
+						bonusMusic2.play();
+					}
+				}		
 			}
 		}());
 		(function animate3(now2){
+			let bonusBox1 = document.querySelectorAll('.reel3 .bonus_how img');
 			if (!start3) start3 = now2;
 			let t = now2 - start3 || 0;
-				p=$reels[2].scrollTop = ((speeds[2] / tMax4 / 10 * (tMax4 - t) * (tMax4 - t) + r[3]) % height)+105 | 0;
+				p=$reels[2].scrollTop = ((speeds[2] / tMax4 / 10 * (tMax4 - t) * (tMax4 - t) + r[3]) % height)+98 | 0;
 			if (t < tMax4)
 				requestAnimationFrame(animate3);
 			else {
 				start3 = undefined;
-
+				for(i=0;i<4;i++){
+					if(bonusBox1[i].outerHTML=="<img src=\"img/bonus.png\">"){
+						bonusMusic3.play();
+					}
+				}		
 			}
 		}());
 		(function animate4(now2){
+			let bonusBox1 = document.querySelectorAll('.reel4 .bonus_how img');
 			if (!start4) start4 = now2;
 			let t = now2 - start4 || 0;
-				p=$reels[3].scrollTop = ((speeds[3] / tMax5 / 10 * (tMax5 - t) * (tMax5 - t) + r[4]) % height)+105 | 0;	
+				p=$reels[3].scrollTop = ((speeds[3] / tMax5 / 10 * (tMax5 - t) * (tMax5 - t) + r[4]) % height)+98 | 0;	
 			if (t < tMax5)
 				requestAnimationFrame(animate4);
 			else {
 				start4 = undefined;
-
+				for(i=0;i<4;i++){
+					if(bonusBox1[i].outerHTML=="<img src=\"img/bonus.png\">"){
+						bonusMusic4.play();
+					}
+				}		
 			}
 		}());
 		(function animate5(now2){
+			let bonusBox1 = document.querySelectorAll('.reel5 .bonus_how img');
 			if (!start5) start5 = now2;
 			let t = now2 - start5 || 0;
-				p=$reels[4].scrollTop = ((speeds[4] / tMax / 10 * (tMax - t) * (tMax - t) + r[4]) % height)+105 | 0;		
+				p=$reels[4].scrollTop = ((speeds[4] / tMax / 10 * (tMax - t) * (tMax - t) + r[4]) % height)+98 | 0;		
 			if (t < tMax+50)
 				requestAnimationFrame(animate5);
 			else {
 				start5 = undefined;
+				for(i=0;i<4;i++){
+					if(bonusBox1[i].outerHTML=="<img src=\"img/bonus.png\">"){
+						bonusMusic5.play();
+					}
+				}		
 			}
-		}());
-		
+		}());		
 		setTimeout(function(){
 			check();
 		},4000)
 }
+
 	function check(){ 
 		let bonusBoxs = document.querySelectorAll('.bonus_how img');
 		let c =[];
@@ -177,7 +204,7 @@ var sm = (function(undefined){
 			if(bonusBoxs[i].outerHTML == "<img src=\"img/bonus.png\">"){
 				bonusBoxs[i].classList.add("congrats");
 				if(bonusBoxs[i].classList.value == "congrats"){
-					 c.push(bonusBoxs[i])				
+					c.push(bonusBoxs[i])				
 				}				
 			}			
 		}
@@ -188,13 +215,14 @@ var sm = (function(undefined){
 	}
 
 	function popup(num){		
-
+		popupMusic.load();
+		popupMusic.play();
 		const boomSpinning = [
 			{ transform: 'scale(0.95)' },
 			{ transform: ' scale(1)' },
 			{ transform: ' scale(0.93)' },
 			{ transform: ' scale(1)' }
-		  ];
+		];
 		const boomTiming = {
 			duration: 200,
 			iterations: 1,
@@ -206,47 +234,54 @@ var sm = (function(undefined){
 		light.style['display'] ='block';
 		prize.animate(boomSpinning,boomTiming)
 		prize.style.display = "block";
-		if(num >=7){
-			prize.innerHTML = 
-		`	<div class="popup">
-				<p> Getting the *Big Gifts* !!</p>
-			</div>
-			`
-		;
-		}else if(num <7){
-			prize.innerHTML = 
-		`	<div class="popup">
-				<p> Getting the *Gifts* !!</p>
-			</div>
-			`
+		// 禮物個數對應獎項
+		switch(num) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+				prize.innerHTML = 
+				`
+					<div class="popup">
+						<p>Getting the *Gifts* !!</p>
+					</div>
+				`	
+			break;
+			case 6:
+			case 7:
+				prize.innerHTML = 
+				`
+					<div class="popup">
+						<p>Getting the *Big Gifts* !!</p>
+					</div>
+				`
+			break;
+			case 8:
+			case 9:
+				prize.innerHTML = 
+				`
+					<div class="popup">
+						<p>Getting the *Awesome Gifts* !!</p>
+					</div>
+				`	
+			break;
+			default:
+				prize.innerHTML = 
+				`
+					<div class="popup">
+						<p>Getting the *Special Gifts* !!</p>
+					</div>
+				`				
 		}
-
-		// cancelpop(); //<button class="cancelBtn">Close</button>
-	}
-
-	// function cancelpop(){
-	// 	let btn = document.querySelector('.cancelBtn');
-	// 	btn.addEventListener('click', function (e) {
-	// 		let prize = document.querySelector('.prize');
-	// 		const boomOut = [
-	// 			{ transform: 'scale(1)' },
-	// 			{ transform: ' scale(0.95)' },
-	// 			{ transform: ' scale(0.99)' },
-	// 			{ transform: ' scale(0.92)' },
-	// 			{ transform: ' scale(0.98)' },
-	// 			{ opacity: 0}
-	// 		  ];
-	// 		let fadeout = prize.animate(boomOut,{
-	// 			duration: 400,
-	// 			easing: "ease-in",
-	// 		})
-	// 		fadeout.onfinish = ()=>{
-	// 			initState()
-	// 			prize.style.display = "none"
-	// 		}
-	// 	})
-	// }	
+	}	
 	return {init: init}
 })();
 
 $(sm.init);
+
+window.onclick = function(e){
+	var bgm = new Audio("music/mainform_track.mp3");
+	bgm.play();	
+	bgm.loop = true;
+}
